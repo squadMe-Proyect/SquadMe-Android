7de5cbe23fb16ebcc768f09137885f25e7dd7cea
daluga0503 <daniel.luquegallardo@gmail.com>
2024-05-02 18:58:49 +0200

    Fragmento de creación de partidos

--- app/src/main/java/com/example/squadme/MainActivity/matches/matchesCreation/MatchCreationFragment.kt
+++ app/src/main/java/com/example/squadme/MainActivity/matches/matchesCreation/MatchCreationFragment.kt
@@ -1,19 +1,25 @@
 package com.example.squadme.MainActivity.matches.matchesCreation
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.navigation.fragment.findNavController
 import com.example.squadme.R
 import com.example.squadme.databinding.FragmentMatchCreationBinding
 import com.example.squadme.databinding.FragmentPlayerCreationBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 
 @AndroidEntryPoint
 class MatchCreationFragment : Fragment() {
     private lateinit var binding: FragmentMatchCreationBinding
+    private val calendar: Calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +35,60 @@ class MatchCreationFragment : Fragment() {
         binding.Cancelar.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        binding.btnFecha.setOnClickListener {
+            showDatePicker()
+        }
+
+        binding.btnHora.setOnClickListener {
+            showTimePicker()
+        }
+    }
+
+
+    private fun showDatePicker() {
+        val dateListener = DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateEditText()
+        }
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            dateListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    private fun showTimePicker() {
+        val timeListener = TimePickerDialog.OnTimeSetListener { _: TimePicker, hourOfDay: Int, minute: Int ->
+            calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+            calendar.set(Calendar.MINUTE, minute)
+            updateTimeEditText()
+        }
+
+        val timePickerDialog = TimePickerDialog(
+            requireContext(),
+            timeListener,
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        )
+        timePickerDialog.show()
+    }
+
+    private fun updateDateEditText() {
+        val formattedDate = "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.YEAR)}"
+        binding.editTextFecha.setText(formattedDate)
+    }
+
+    private fun updateTimeEditText() {
+        val formattedTime = "${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}"
+        binding.editTextTHora.setText(formattedTime)
     }
 
 }
